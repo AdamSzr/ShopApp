41c363e8906b3295a197f0206a32ec2ef72d16ed
AdamSzr <adam.szr98@gmail.com>
2022-01-27 16:38:23 +0100

    updated urls

--- ShopApp/app/config.ts
+++ ShopApp/app/config.ts
@@ -1,4 +1,4 @@
-export const URL_ALL_PROD = `http://192.168.0.18:3001/api/products`
-export const URL_INCREMENT_PROD = `http://192.168.0.18:3001/api/product/decrement`
+export const URL_ALL_PROD = `http://apirte-labproj23.apps.ocp.lab.cloudpak.site/api/products`
+export const URL_INCREMENT_PROD = `http://apirte-labproj23.apps.ocp.lab.cloudpak.site/api/product/decrement`
 
   
--- WarehouseApp/app/config.ts
+++ WarehouseApp/app/config.ts
@@ -1,9 +1,6 @@
-// export const URL_ALL_PROD = `http://localhost:3001/api/products`
-// export const URL_INCREMENT_PROD = `http://localhost:3001/api/product/increment`
+export const URL_CREATE_PRODUCT = `http://apirte-labproj23.apps.ocp.lab.cloudpak.site/api/product/new`
 
-export const URL_CREATE_PRODUCT = `http://192.168.0.18:3001/api/product/new`
-
-export const URL_ALL_PROD = `http://192.168.0.18:3001/api/products`
-export const URL_INCREMENT_PROD = `http://192.168.0.18:3001/api/product/increment`
+export const URL_ALL_PROD = `http://apirte-labproj23.apps.ocp.lab.cloudpak.site/api/products`
+export const URL_INCREMENT_PROD = `http://apirte-labproj23.apps.ocp.lab.cloudpak.site/api/product/increment`
 
   
